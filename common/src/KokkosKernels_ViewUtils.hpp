40a3662e4e5115e2f9ff38261cae1fcda982c974
Carl Pearson <cwpears@sandia.gov>
2023-05-05 16:56:02 -0600

    add KokkosKernels_ViewUtils.hpp

---
+++ common/src/KokkosKernels_ViewUtils.hpp
@@ -0,0 +1,46 @@
+//@HEADER
+// ************************************************************************
+//
+//                        Kokkos v. 4.0
+//       Copyright (2022) National Technology & Engineering
+//               Solutions of Sandia, LLC (NTESS).
+//
+// Under the terms of Contract DE-NA0003525 with NTESS,
+// the U.S. Government retains certain rights in this software.
+//
+// Part of Kokkos, under the Apache License v2.0 with LLVM Exceptions.
+// See https://kokkos.org/LICENSE for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//@HEADER
+
+#ifndef KOKKOSKERNELS_VIEWUTILS_HPP
+#define KOKKOSKERNELS_VIEWUTILS_HPP
+#include "Kokkos_Core.hpp"
+
+namespace KokkosKernels {
+
+namespace Impl {
+
+
+template <typename View>
+auto make_unmanaged(const View &v) {
+
+    using data_type = typename View::data_type;
+    using layout_type = typename View::array_layout;
+    using memory_space = typename View::memory_space;
+
+    using orig_traits = typename View::memory_traits;
+    constexpr unsigned new_traits = orig_traits::impl_value | Kokkos::Unmanaged;
+
+    using unmanaged_type = Kokkos::View<data_type, layout_type, memory_space, 
+      Kokkos::MemoryTraits<new_traits>
+    >;
+    return unmanaged_type(v);
+}
+
+
+}  // namespace Impl
+}  // namespace KokkosKernels
+
+#endif
